6cfa7051ead668525ebf5fb74388e297105e1208
brenosekiro <b88660501@gmail.com>
2020-05-23 21:51:42 -0300

    gridWidgets

--- frontend/src/components/Profile/Profile.tsx
+++ frontend/src/components/Profile/Profile.tsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import './Profile.css'
 
+
 const Profile: React.FC = () => {
     return (
         <div className="profile-wrapper">
-            <div className="top-bar"></div>
-            
+            <div className="top-bar">
+                <div className="weed"></div>
+                <h3>Profile Name</h3>
+                <div className="weed"></div>
+            </div>
+            <div className="widgets-grid">
+                <div className="map"></div>
+                <div className="askings"></div>
+                <div className="pendingAskings"></div>
+            </div>
         </div>
     )
 }
