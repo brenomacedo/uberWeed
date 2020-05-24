e3ddb8e827242558896ce4db2aa9271dea197b47
brenosekiro <b88660501@gmail.com>
2020-05-23 22:50:44 -0300

    map

--- frontend/src/components/Profile/Profile.tsx
+++ frontend/src/components/Profile/Profile.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import './Profile.css'
+import { GoogleApiWrapper, Map, GoogleAPI } from 'google-maps-react'
 
+interface IMapProps{
+    google: GoogleAPI
+}
 
-const Profile: React.FC = () => {
+const Profile: React.FC<IMapProps> = props => {
     return (
         <div className="profile-wrapper">
             <div className="top-bar">
@@ -11,12 +15,23 @@ const Profile: React.FC = () => {
                 <div className="weed"></div>
             </div>
             <div className="widgets-grid">
-                <div className="map"></div>
-                <div className="askings"></div>
-                <div className="pendingAskings"></div>
+                <div className="map">
+                    <Map containerStyle={{
+                        position: 'relative',  
+                        width: '100%',
+                        height: '100%' }}  google={props.google} />
+                </div>
+                <div className="askings">
+
+                </div>
+                <div className="pendingAskings">
+
+                </div>
             </div>
         </div>
     )
 }
 
-export default Profile
+export default GoogleApiWrapper({
+    apiKey: process.env.REACT_APP_API_KEY || ''
+})(Profile)
