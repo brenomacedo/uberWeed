a4786420cfbfd8e06b90169e11b882fdaa51ea6c
brenosekiro <b88660501@gmail.com>
2020-05-25 21:52:57 -0300

    authMiddleware

---
+++ backend/src/middlewares/auth.ts
@@ -0,0 +1,27 @@
+import { Request, Response, NextFunction } from 'express'
+import { key } from '../auth.json'
+import jwt from 'jsonwebtoken'
+
+export default (req: Request, res: Response, next: NextFunction) => {
+    const authHeader = req.headers.authorization
+
+    if(!authHeader)
+        return res.status(401).send('NO TOKEN PROVIDED!')
+    
+    if(authHeader?.split(' ').length !== 2)
+        return res.status(401).send('TOKEN FORMAT IS INVALID')
+    
+    if(!authHeader?.match(/Bearer/))
+        return res.status(401).send('TOKEN FORMAT IS INVALID')
+
+    const token = authHeader.split(' ')[1]
+
+    jwt.verify(token, key, (err, decoded) => {
+        if(err) {
+            res.status(401).send('INVALID TOKEN')
+        }
+        
+        req.query.userId = String(decoded)
+        return next()
+    })
+}
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,10 +1,11 @@
 import { Router, Request, Response } from 'express'
 import userController from './controllers/userController'
 import authController from './controllers/authController'
+import authMiddleware from './middlewares/auth'
 const routes = Router()
 
 routes.post('/user/create', userController.create)
-routes.get('/user/select', userController.select)
+routes.get('/user/select', authMiddleware, userController.select)
 routes.get('/authenticate', authController.authenticate)
 
 export default routes
